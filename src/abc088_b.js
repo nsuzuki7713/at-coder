18e37130e56c8206987e8352b629eefd9ab90ae1
nsuzuki7713 <nsuzuki7713@gmail.com>
2020-03-14 22:55:16 +0900

    abc088_b.js

---
+++ src/abc088_b.js
@@ -0,0 +1,29 @@
+// https://atcoder.jp/contests/abc088/tasks/abc088_b
+"use strict"
+function Main(input) {
+  const params = input.split('\n');
+  const n = Number(params[0]);
+  const a = params[1].split(' ').map(Number);
+  const sortA = a.sort((s1, s2) => s2 - s1);
+  
+  const aliceTotal = sortA.reduce((acc, cur, index) => {
+    if (index % 2 === 0) {
+      return acc + cur;
+    }
+    
+    return acc;
+  }, 0);
+
+  const bobTotal = sortA.reduce((acc, cur, index) => {
+    if (index % 2 !== 0) {
+      return acc + cur;
+    }
+    
+    return acc;
+  }, 0);
+
+  console.log(aliceTotal - bobTotal);
+
+}
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+Main(require("fs").readFileSync("/dev/stdin", "utf8"));
